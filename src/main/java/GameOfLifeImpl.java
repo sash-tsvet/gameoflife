43ec257667de82135633baf5e2ea191918aad90c
Aleksandra Tsvetkova <sash.tsvet@gmail.com>
2017-12-17 16:44:38 +0300

    initial project version

---
+++ src/main/java/GameOfLifeImpl.java
@@ -0,0 +1,187 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//import static jdk.internal.jshell.tool.Startup.readFile;
+
+/**
+ * @author Vasyukevich Andrey
+ * @since 12.12.2017
+ */
+public class GameOfLifeImpl implements GameOfLife {
+    int size=0;
+    int iterations=0;
+
+    Boolean[] curGen = new Boolean[0];
+    Boolean[] nextGen = new Boolean[0];
+
+    @Override
+    public List<String> play(String inputFile) {
+
+        try(BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line = br.readLine();
+
+            String[] words = line.split(" ");
+
+            size = Integer.parseInt(words[0]);
+            iterations = Integer.parseInt(words[1]);
+            curGen = new Boolean[size*size];
+
+            int c;
+            int pos = 0;
+            while ((c = br.read()) != -1) {
+                //Since c is an integer, cast it to a char. If it isn't -1, it will be in the correct range of char.
+                switch ((char)c) {
+                    case '0':
+                        curGen[pos] = false;
+                        pos ++;
+                        break;
+                    case  '1':
+                        curGen[pos] = true;
+                        pos ++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        // initialises field for drawing cells
+//        StdDraw.setCanvasSize(size, size);
+//        StdDraw.setYscale(0, size);
+//        StdDraw.setXscale(0, size);
+//        StdDraw.setPenRadius(0);
+//        StdDraw.setPenColor(StdDraw.GREEN);
+
+        long start = System.currentTimeMillis();
+        long time = System.currentTimeMillis() - start;
+
+        // infinitely draws field
+        while (iterations>0) {
+            curGen = countNextGen(size, size);
+            iterations--;
+//            StdDraw.show(0);
+//            StdDraw.clear();
+//            for (int row = 0; row < size; row++) {
+//                for (int col = 0; col < size; col++) {
+//                    if (curGen[row*size + col] == true) {
+//                        StdDraw.point(col, row);
+//                    }
+//                }
+//            }
+//            StdDraw.show(0);
+            System.out.println(System.currentTimeMillis() - time + "ms, iterations left: " + iterations);
+            time = System.currentTimeMillis();
+        }
+
+        time = System.currentTimeMillis() - start;
+        System.out.println(time + "ms");
+
+        List<String> returnvalue = new ArrayList<String>();
+        for (int i = 0; i < size; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < size; j++) {
+                if (curGen[i*size+j]) sb.append("1");
+                else sb.append("0");
+            }
+            returnvalue.add(sb.toString());
+        }
+
+
+        return returnvalue;
+    }
+
+    int parallelevel = 100;
+    public Boolean[] countNextGen(int rowsNum, int colsNum) {
+
+        nextGen = new Boolean[rowsNum*colsNum];
+        System.arraycopy( curGen, 0, nextGen, 0, rowsNum*colsNum );
+
+
+        SubUpdateThread[] subUpdaters = new SubUpdateThread[rowsNum/parallelevel + 1];
+        for(int i = 0; i < rowsNum/parallelevel + 1; i++){
+            subUpdaters[i] = new SubUpdateThread(i);
+            subUpdaters[i].start();
+        }
+
+        for(int i = 0; i < subUpdaters.length; i++){
+            try {
+                subUpdaters[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return nextGen;
+    }
+
+
+    public static int countCellNeighbours(Boolean[] curGen, int rowsNum, int colsNum, int row, int col) {
+        int numOfNeighbours = 0;
+        if (curGen[((row - 1+rowsNum)%rowsNum)*colsNum + (col - 1+colsNum)%colsNum]) {
+            numOfNeighbours++;
+        }
+        if (curGen[((row - 1+rowsNum)%rowsNum)*colsNum + col]) {
+            numOfNeighbours++;
+        }
+        if (curGen[((row - 1+rowsNum)%rowsNum)*colsNum + (col + 1)%colsNum]) {
+            numOfNeighbours++;
+        }
+        if (curGen[row*colsNum + (col - 1+colsNum)%colsNum]) {
+            numOfNeighbours++;
+        }
+        if (curGen[row*colsNum + (col + 1)%colsNum]) {
+            numOfNeighbours++;
+        }
+        if (curGen[((row + 1)%rowsNum)*colsNum + (col - 1+colsNum)%colsNum]) {
+            numOfNeighbours++;
+        }
+        if (curGen[((row + 1)%rowsNum)*colsNum + col]) {
+            numOfNeighbours++;
+        }
+        if (curGen[((row + 1)%rowsNum)*colsNum + (col + 1)%colsNum]) {
+            numOfNeighbours++;
+        }
+
+        return numOfNeighbours;
+    }
+
+    private class SubUpdateThread extends Thread{
+        private int iNum;
+
+        public SubUpdateThread(int iNum){
+            this.iNum = iNum;
+        }
+
+        @Override
+        public void run() {
+
+            for (int row = iNum*parallelevel; (row < (iNum+1)*parallelevel)&&(row<size); row++) {
+                for (int col = 0; col < size; col++) {
+
+                    int numOfNeighbours = countCellNeighbours(curGen, size, size, row, col);
+
+                    // under or overpopulation, cell dies
+                    if ((numOfNeighbours < 2) || (numOfNeighbours > 3)) {
+                        nextGen[row*size + col] = false;
+                    }
+
+                    // cell lives on to next generation
+                    if (numOfNeighbours == 2) {
+                        nextGen[row*size + col] = curGen[row*size + col];
+                    }
+
+                    // cell either stays alive, or is born
+                    if (numOfNeighbours == 3) {
+                        nextGen[row*size + col] = true;
+                    }
+                }
+            }
+        }
+    }
+}
